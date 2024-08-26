6d87397cf77dddf43712b6a26162b672a4a8a6e3
Shreyaskharche2003 <113256337+Shreyaskharche2003@users.noreply.github.com>
2024-08-26 10:39:50 +0530

    Update shipment_st.py

--- shipment_st.py
+++ shipment_st.py
@@ -3,45 +3,55 @@ import pandas as pd
 import streamlit as st
 import pickle
 
-pickled_model=pickle.load(open("/home/shreyas2003/Downloads/Shipment_price/trained_model3.sav",'rb'))
+# Load the model
+pickled_model = pickle.load(open("/home/shreyas2003/Downloads/Shipment_Price_Prediction-main/trained_model3.sav", 'rb'))
 
-
-def shipment_price_prdiction(b):
-    input_val =b
+def shipment_price_prediction(b):
+    input_val = b
     final_features = [np.array(input_val)]
     dataframe1 = pd.DataFrame(final_features)
 
-    output = pickled_model.predict(dataframe1 )
+    output = pickled_model.predict(dataframe1)
     result = "%.2f" % round(output[0], 2)
     return result
 
 def main():
-    #title
-    st.title("Shipment_Price_Prediction")
+    # Title
+    st.title("Shipment Price Prediction")
 
-    #taking input from user
-    Unit_of_Measure=st.text_input("Unit of Measure (per Pack)")
-    Line_item_quantity=st.text_input("Line Item Quantity")
-    pack_price=st.text_input("Pack Price")
-    unit_price=st.text_input("Unit Price")
-    freight_cost=st.text_input("Freight Cost (USD $)")
-    Line_Item_insurance=st.text_input("Line Item Insurance (USD $)")
-    shipment_mode_Ocean=st.text_input("shipment_mode_Ocean")
-    shipment_mode_Truck=st.text_input("shipment_mode_Truck")
-    shipment_mode_Air=st.text_input("shipment_mode_Air")
-    shipment_mode_Air_charter=st.text_input("shipment_mode_Air charter")
+    # Taking input from user
+    Unit_of_Measure = st.text_input("Unit of Measure (per Pack)")
+    Line_item_quantity = st.text_input("Line Item Quantity")
+    pack_price = st.text_input("Pack Price")
+    unit_price = st.text_input("Unit Price")
+    freight_cost = st.text_input("Freight Cost (USD $)")
+    Line_Item_insurance = st.text_input("Line Item Insurance (USD $)")
 
+    # Binary input for shipment modes
+    shipment_mode_Ocean = st.radio("Shipment Mode: Ocean", [0, 1])
+    shipment_mode_Truck = st.radio("Shipment Mode: Truck", [0, 1])
+    shipment_mode_Air = st.radio("Shipment Mode: Air", [0, 1])
+    shipment_mode_Air_charter = st.radio("Shipment Mode: Air Charter", [0, 1])
 
-
-    # code for prediction
-    price_prediction=""
+    # Code for prediction
+    price_prediction = ""
 
     # Creating a Button
     if st.button("Predict"):
-        price_prediction=shipment_price_prdiction([Unit_of_Measure,Line_item_quantity,pack_price,unit_price,freight_cost,Line_Item_insurance])
+        price_prediction = shipment_price_prediction([
+            Unit_of_Measure,
+            Line_item_quantity,
+            pack_price,
+            unit_price,
+            freight_cost,
+            Line_Item_insurance,
+            shipment_mode_Ocean,
+            shipment_mode_Truck,
+            shipment_mode_Air,
+            shipment_mode_Air_charter
+        ])
     
     st.success(price_prediction)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
